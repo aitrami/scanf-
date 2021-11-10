6817aa2d6badc53a4ce07dfc0b6399d73b7158f1
mohamed aitrami <mohamed.aitrami1999@gmail.com>
2021-11-10 16:33:52 +0100

    corrected the project indexing

--- Affichage Informations/main.c
+++ Affichage Informations/main.c
@@ -3,6 +3,20 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    printf("veuillez entrer la valeur de x : ");
+    int x ;
+    scanf("%d"&x);
+    printf("X = %d \n",x);
+    float a , b ;
+    printf("veuillez entrer les valeur de a et b : ");
+    scanf("%f%f", &a,&b);
+    printf("A = %.2f , B = %.2f",a,b);
+    printf("A + B = %.2f",a+b);
+
+
+
+
+
     return 0;
+
 }
