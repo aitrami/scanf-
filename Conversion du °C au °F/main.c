10b7ebc68bfe7137860044938e4c5d69301eee84
mohamed aitrami <mohamed.aitrami1999@gmail.com>
2021-11-11 14:37:46 +0100

    degré Celsius

---
+++ Conversion du °C au °F/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    float c, fh;
+
+
+    printf("Enter temperature in Fahrenheit\n");
+    scanf("%f",&fh);
+
+   c = (fh - 32) / 1.8;
+
+    printf("Température in Degree Celius is %f\n", c);
+
+
+    return 0;
+}
